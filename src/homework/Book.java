c2279d743ccf44e21db5d639b28162b7447794f9
De93m <DmitryKorshunov93@gmail.com>
2023-09-28 16:26:30 +0300

    objects 2

--- src/homework/Book.java
+++ src/homework/Book.java
@@ -2,24 +2,33 @@ package homework;
 
 public class Book {
 
-    String bookTitle;
-    int age;
+    private String bookTitle;
+    private int age;
 
     public Book(String bookTitle, int age) {
         this.bookTitle = bookTitle;
+        this.age = age;
+    }
+    public String getBookTitle(){
+        return this.bookTitle;
+    }
+    public Integer getAge(){
+        return this.age;
+    }
+    public void setAge (int age){
         this.age = age;
     }
 
     public static void getBookAuthor() {
         Author bookOne = new Author("George", "Orwell");
 
-        System.out.print("Author = " + bookOne.name + " " + bookOne.surname + ", ");
+        System.out.print("Author = " + bookOne.getName() + " " + bookOne.getSurname() + ", ");
     }
 
     public static void getBookAuthor1() {
         Author bookTwo = new Author("Aldous", "Huxley");
 
-        System.out.print("Author = " + bookTwo.name + " " +  bookTwo.surname);
+        System.out.print("Author = " + bookTwo.getName() + " " +  bookTwo.getSurname());
     }
 
 }
--- src/homework/Main.java
+++ src/homework/Main.java
@@ -9,14 +9,16 @@ public class Main {
 
         Book bookOne = new Book("Animal farm", 1945);
 
-        System.out.print("Book Title = " + bookOne.bookTitle +", ");
-        System.out.println("Years = " + bookOne.age);
+        System.out.print("Book Title = " + bookOne.getBookTitle() +", ");
+        System.out.println("Years = " + bookOne.getAge());
+        bookOne.setAge(1946);
+        System.out.println("Years = " + bookOne.getAge());
 
         Book.getBookAuthor1();
 
         Book bookTwo = new Book("Brave New World",1932);
-        System.out.print("Book Title = " + bookTwo.bookTitle +", ");
-        System.out.println("Years = " + bookTwo.age);
+        System.out.print("Book Title = " + bookTwo.getBookTitle() +", ");
+        System.out.println("Years = " + bookTwo.getAge());
 
 
 
